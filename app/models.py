22a78bc69583c6fa14002e3f5eadd7a6b9a66190
Sungwon Chung <sungpia@me.com>
2019-05-24 14:34:47 -0700

    Scaffolded app

--- app/models.py
+++ app/models.py
@@ -7,7 +7,16 @@ from django.db import models
 
 
 class Message(models.Model):
+	MESSAGE_TYPES = (
+		('D', 'Debug'),
+		('I', 'INFO'),
+	)
 	source = models.TextField()
-	message_type = models.CharField(max_length=16)
+	message_type = models.CharField(
+		max_length=1,
+		choices=MESSAGE_TYPES,
+		blank=True,
+		default='D',
+	)
 	time_stamp = models.DateField(auto_now=True)
 	message = models.TextField()
--- app/views.py
+++ app/views.py
@@ -2,13 +2,40 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Message
+
+from django.template import loader
 
 def index(request):
 	return HttpResponse("Hello World")
 
-def detail(request):
-	return HttpResponse("Detail")
+@csrf_exempt
+def message(request):
+	msgg = None 
+	if request.method == "POST":
+		# TODO(sungpia@me.com): Handle data.
+		print(request.POST)
+		print(request.POST.get('source'))
+		try:	
+			msg = Message()
+			msg.source = request.POST.get('source', '')
+			msg.message_type = request.POST.get('message_type', '')
+			msg.message = request.POST.get('message', '')
+			msg.save()	
+		except:
+			print('data not saved')
+		return HttpResponse(status=200)
+	elif request.method == "GET":
+		msgg = Message.objects.all()
+		context = {
+			'msg': msgg,
+		}
+		template = loader.get_template('app/index.html')
+		print(context)
+		return HttpResponse(template.render(context, request))
+	return HttpResponse(status=501)
